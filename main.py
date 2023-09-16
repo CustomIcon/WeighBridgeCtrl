5ebd5dc3803db0099c61411ae5fb3f6c2d79c8b8
ポキ <custom.icon@vk.com>
2023-09-16 18:12:29 +0500

    documenting the thing

--- main.py
+++ main.py
@@ -6,17 +6,22 @@ import contextlib
 from time import sleep
 import re
 
+# version tag
 __version__ = '0.1'
 
+
+# importing config.ini file
 config = ConfigParser()
 config.read('config.ini')
 
+# getting USB serial connection established
 ser = serial.Serial(
     config.get('settings', 'COM'), baudrate=config.getint('settings', 'baud'), timeout=1, parity="N", stopbits=1
 )
+# memory cache
 cache = {'pushClicked': False}
 
-
+# sanatize read data from USB serial port to the application
 def sanitize(in_bin, result=""):
     with contextlib.suppress(IndexError):
         bb = in_bin.decode(encoding="ascii", errors="ignore")
@@ -24,7 +29,9 @@ def sanitize(in_bin, result=""):
         return clean[0][:-2]
 
 
+# main page
 def main(page: ft.Page):
+    # push button function
     def pushClick(e):
         page.snack_bar = ft.SnackBar(
             content=ft.Text("Data already pushed!"),
@@ -40,6 +47,7 @@ def main(page: ft.Page):
         #         headers={'User-Agent': 'Mozilla/5.0'}
         #     )
         # ).read()
+    # copy button function
     def copyClick(e):
         e.page.set_clipboard(weight_view.value)
         page.snack_bar = ft.SnackBar(
@@ -48,6 +56,7 @@ def main(page: ft.Page):
         )
         page.snack_bar.open = True
         return page.update()
+    # all buttons on page
     def Buttons():
         return [
             ft.Container(
@@ -58,6 +67,7 @@ def main(page: ft.Page):
             ),
         ]
 
+    # page preference
     page.window_width = 300
     page.window_height = 200
     page.window_resizable = False
@@ -77,6 +87,7 @@ def main(page: ft.Page):
         ),
         weight_view,
     )
+    # a loop reading data coming from serial port
     while True:
         data = ser.read_all()
         if data:
@@ -86,4 +97,5 @@ def main(page: ft.Page):
                 weight_view.value = sanitize(data)
                 page.update()
 
+# RUN IT BACK
 ft.app(main, assets_dir="assets", name="Weight Reader")
